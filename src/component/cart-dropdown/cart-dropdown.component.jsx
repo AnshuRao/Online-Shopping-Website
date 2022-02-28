bb6850ab013e29ab1d87c4017f5472eaef5b95b7
AnshuRao <rao.anshu38@gmail.com>
2022-02-28 18:24:16 +0530

    adding selector liberary and updating the redux throung selector

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -4,13 +4,27 @@ import CustomButton from '../custom-button/custome-button.component';
 
 import './cart-dropdown.style.scss';
 
-const CartDropDown = ()=>{
+import CartItem from "../cart-item/cart-item.component";
+
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selector";
+
+
+const CartDropDown = ({cartItems})=>{
     return(
         <div className="cart-dropdown">
-            <div className="cart-item"/>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem=> <CartItem key={cartItem.id} item= {cartItem}/>)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
- 
-export default CartDropDown;
+
+const mapStateToProps =(state)=>({
+    cartItems : selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropDown);
